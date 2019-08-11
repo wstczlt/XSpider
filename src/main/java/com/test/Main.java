0bc9a392062102e60076d85b3ced71d6f87cbfcd
zhulantian <zhulantian@kuaishou.com>
2019-08-11 17:37:25 +0800

    continue.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,14 +1,14 @@
 package com.test;
 
-import com.test.dszuqiu.DsHelper;
+import com.test.dszuqiu.DsSpider;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    // DsSpider.runSt(240000, 640000);
+    DsSpider.runSt(240000, 588848);
 
-    DsHelper.read("/Users/Jesse/Desktop/odd");
+    // DsHelper.read("/Users/Jesse/Desktop/odd");
 
     // QueryHelper.queryLeagues();
 
--- src/main/java/com/test/dszuqiu/DsHelper.java
+++ src/main/java/com/test/dszuqiu/DsHelper.java
@@ -1,6 +1,7 @@
 package com.test.dszuqiu;
 
 import static com.test.tools.Utils.isSkip;
+import static com.test.tools.Utils.setSkip;
 import static com.test.tools.Utils.valueOfInt;
 
 import java.io.File;
@@ -13,8 +14,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.http.util.TextUtils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.test.Config;
 import com.test.dszuqiu.parser.OddParser;
+import com.test.dszuqiu.parser.RaceParser;
 import com.test.pipeline.DbPipeline;
 
 public class DsHelper {
@@ -41,30 +47,53 @@ public class DsHelper {
     for (String name : sorted) {
       final Map<String, String> items = new HashMap<>();
 
-      // // 处理Race信息
-      // File raceFile = new File(filepath, name + ".txt");
-      // String raceRawText = FileUtils.readFileToString(raceFile, "utf-8");
-      // new RaceParser(raceRawText, items).doParse();
+      // 处理Race信息
+      File raceFile = new File(filepath, name + ".txt");
+      if (raceFile.isFile()) {
+        String raceRawText = FileUtils.readFileToString(raceFile, "utf-8");
+        JSONObject json = JSON.parseObject(raceRawText);
+        String error = RaceParser.isLegalOdd(json);
+        if (!TextUtils.isEmpty(error)) {
+          setSkip(items);
+          Config.LOGGER.log(
+              String.format("[%s], matchID=%d, Error=%s", "RaceJob", -1, error));
+          return;
+        }
+
+        new RaceParser(raceRawText, items).doParse();
+      }
+
 
       // 处理ODD指数信息
       File oddFile = new File(filepath, name + "_odd.txt");
       if (oddFile.isFile()) {
         String oddRawText = FileUtils.readFileToString(oddFile, "utf-8");
+        JSONObject json = JSON.parseObject(oddRawText);
+        String error = RaceParser.isLegalRace(json);
+        if (!TextUtils.isEmpty(error)) {
+          setSkip(items);
+          Config.LOGGER.log(
+              String.format("[%s], matchID=%d, Error=%s", "OddJob", -1, error));
+          return;
+        }
+
         new OddParser(oddRawText, items).doParse();
       }
 
+      
+
       // 写入Database
       if (!isSkip(items)) {
-//        pipeline.process(items);
-         sKeys.addAll(items.keySet());
-         sInt++;
+        // pipeline.process(items);
+        sKeys.addAll(items.keySet());
+        sInt++;
 
-         if (sInt >= 1000) {
-         sKeys.stream().distinct().sorted()
-         .filter(s -> !s.equalsIgnoreCase("matchID"))
-         .forEach(s -> System.out.print(s + ","));
-         return;
-         }
+        if (sInt >= 1000) {
+          sKeys.stream().distinct().sorted()
+              .filter(s -> !s.equalsIgnoreCase("matchID"))
+              .forEach(s -> System.out.print(s + ","));
+          return;
+        }
       }
     }
   }
