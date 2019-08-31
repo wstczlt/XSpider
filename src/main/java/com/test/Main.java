81299211c6e2ee0755eaef068348d39e0bf743ce
zhulantian <zhulantian@kuaishou.com>
2019-08-31 17:05:17 +0800

    简单重构测试模块.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -8,52 +8,33 @@ public class Main {
 
     Config.class.getSimpleName();
 
-    // DsSpider.runSt(240000, 588848);
-
-    // DsHelper.read("/Users/Jesse/Desktop/odd"); // ~511531
-
-
-    // QueryHelper.queryLeagues();
-
-    // Radar.main(null);
-    //
     // new RuleFactory(RuleType.SCORE).build();
     // new RuleFactory(RuleType.BALL).build();
-    //
 
-    // HistoryTester.testDisplay();
-    //
-    // System.out.println("\n昨日数据: ");
-    // HistoryTester.testOnLastDay();
-    //
-    // System.out.println("\n近3日数据: ");
-    // HistoryTester.testOnLast3Day();
-    //
-    // System.out.println("\n上周数据: ");
-    // HistoryTester.testOnLast1Weeks();
-    //
-    // System.out.println("\n上两周数据: ");
-    // HistoryTester.testOnLast2Weeks();
+
+    System.out.println("\n昨日数据: ");
+    HistoryTester.testAndEval(0, 1);
+
+    System.out.println("\n近3日数据: ");
+    HistoryTester.testAndEval(0, 3);
+
+    System.out.println("\n上周数据: ");
+    HistoryTester.testAndEval(0, 7);
+
+    System.out.println("\n上两周数据: ");
+    HistoryTester.testAndEval(0, 14);
+
+    System.out.println("\n上月数据: ");
+    HistoryTester.testAndEval(0, 28);
 
     for (int i = 0; i < 10; i++) {
       System.out.println("\n随机数据(" + i + "): ");
-      HistoryTester.testOnRandomHistoryWeek();
+      HistoryTester.testAndEval();
     }
 
-    // HistoryTester.testHistoryDisplay(3);
-
-    // HistoryTester.testOnNewHistory(3);
-
-    // HistoryTester.testOnNewHistory(3);
-    // HistoryTester.testOnNewHistory(14);
-
-    // HistoryTester.testDisplay();
-    // HistoryTester.testHistoryDisplay(1);
+    // HistoryTester.testAndDisplay(0, 1);
 
     // new HistoryRadar().run(1000);
-    // DsHelper.main(null);
-
-    // HistoryTester.testBotDisplay();
   }
 
 }
