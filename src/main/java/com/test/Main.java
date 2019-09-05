7ad74ef39c86d4897e7c3fea8d8298f275b6aeac
zhulantian <zhulantian@kuaishou.com>
2019-09-05 23:41:35 +0800

    continue.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,6 +1,6 @@
 package com.test;
 
-import com.test.manual.HistoryTester;
+import com.test.manual.HistoryRadar;
 
 public class Main {
 
@@ -12,30 +12,30 @@ public class Main {
     // new RuleFactory(RuleType.BALL).build();
 
     //
-//    System.out.println("\n昨日数据: ");
-//    HistoryTester.testAndEval(0, 1);
-//
-//    System.out.println("\n近3日数据: ");
-//    HistoryTester.testAndEval(0, 3);
-//
-//    System.out.println("\n上周数据: ");
-//    HistoryTester.testAndEval(0, 7);
-//
-//    System.out.println("\n上两周数据: ");
-//    HistoryTester.testAndEval(0, 14);
-//
-//    System.out.println("\n上月数据: ");
-//    HistoryTester.testAndEval(0, 28);
-//
-//    for (int i = 0; i < 10; i++) {
-//      System.out.println("\n随机数据(" + i + "): ");
-//      HistoryTester.testAndEval();
-//    }
+    // System.out.println("\n昨日数据: ");
+    // HistoryTester.testAndEval(0, 1);
+    //
+    // System.out.println("\n近3日数据: ");
+    // HistoryTester.testAndEval(0, 3);
+    //
+    // System.out.println("\n上周数据: ");
+    // HistoryTester.testAndEval(0, 7);
+    //
+    // System.out.println("\n上两周数据: ");
+    // HistoryTester.testAndEval(0, 14);
+    //
+    // System.out.println("\n上月数据: ");
+    // HistoryTester.testAndEval(0, 28);
+    //
+    // for (int i = 0; i < 10; i++) {
+    // System.out.println("\n随机数据(" + i + "): ");
+    // HistoryTester.testAndEval();
+    // }
 
 
-     HistoryTester.fetchAndDisplay(30);
+    // HistoryTester.fetchAndDisplay(30);
 
-    // new HistoryRadar().run(1000);
+    new HistoryRadar().run(10000);
   }
 
 }
